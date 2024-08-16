047e05084df8e1778976877684229e7270c2263a
phcodesage <rechceltoledo@gmail.com>
2024-08-17 00:33:31 +0800

    fixed: projects-mdx

--- components/sections/projects/config.ts
+++ components/sections/projects/config.ts
@@ -7,32 +7,14 @@ const projects: Project[] = [
     slug: 'shamuscoachbus',
     description:
       'Shamus Coach Bus has been providing top-notch transportation services for over 29 years. Family-owned and operated, they bring a personal touch with industry knowledge and experience to ensure comfortable and luxurious travel accommodations for groups and destinations.',
-    thumbnail: '/images/projects/shamuscoachbus/cover.jpg',
-    techstack: [
-      { label: 'Bootstrap' },
-      { label: 'jQuery' },
-      { label: 'Node.js' },
-      { label: 'JavaScript' },
-      { label: 'REST API' }
-    ],
-    role: 'Full Stack Developer',
-    website: 'https://shamuscoachbus.com/',
-    github: 'https://github.com/phcodesage/shamuscoachbus.git'
+    thumbnail: '/images/projects/shamuscoachbus/cover.jpg'
   },
   {
     slug: 'purpleroof',
     name: 'Purple Roof',
     description:
       'A professional website built using modern web technologies to showcase projects and skills.',
-    thumbnail: '/images/projects/purpleroof/cover.jpg',
-    techstack: [
-      { label: 'Next.js' },
-      { label: 'React' },
-      { label: 'Tailwind CSS' },
-      { label: 'AWS S3' },
-      { label: 'API Development' }
-    ],
-    role: 'Junior Front End Developer'
+    thumbnail: '/images/projects/purpleroof/cover.jpg'
   },
   {
     name: 'Ibooky Hotel Reservation',

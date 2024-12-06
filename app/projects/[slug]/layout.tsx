ede63469581a479151057a1534587c5e39deb091
Rechcel Toledo <rechceltoledo@gmail.com>
2024-12-06 16:47:19 +0800

    added: dental-booking-website

--- app/projects/[slug]/layout.tsx
+++ app/projects/[slug]/layout.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import type { Metadata } from 'next';
 
 import { Header } from '@/components/sections';
@@ -5,17 +7,19 @@ import { Header } from '@/components/sections';
 import Preloader from '@/components/preloader/preloader';
 import Cursor from '@/components/cursor/cursor';
 import SmoothScroll from '@/components/smooth-scroll';
+import { useState } from 'react';
 
 export default function ProjectLayout({
   children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [activeSection, setActiveSection] = useState('projects');
+
   return (
     <SmoothScroll>
-      {/* <Preloader /> */}
       <div className="flex min-h-[100dvh] flex-col">
-        <Header activeSection="projects" onNavigate={(section: string) => {}} />
+        <Header activeSection={activeSection} onNavigate={setActiveSection} />
         <main className="my-14 flex-1">{children}</main>
       </div>
       <Cursor />
